5fbad4b4fc7a671637e7905a7791b42a4b06998d
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2023-06-24 21:57:09 +0530

    Time: 475 ms (95.04%), Space: 27.5 MB (81.93%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        hash_map = {}
+        for num in nums:
+            if num in hash_map:
+                return True
+            else:
+                hash_map[num] = num
+        
+        return False
+            
+        
