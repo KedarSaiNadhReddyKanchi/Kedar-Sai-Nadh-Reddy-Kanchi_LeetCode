204227cc075052bc155600f07190ef85df861d91
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2023-06-27 15:10:17 +0530

    Time: 39 ms (70.41%), Space: 13.3 MB (99.41%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -0,0 +1,54 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        
+        cost_length = len(cost)
+        
+        if cost_length == 2:
+            value1 = cost[0]
+            value2 = cost[1]
+            if value1 <= value2:
+                return value1
+            else:
+                return value2
+        
+        if cost_length == 1:
+            return 0
+                
+        topcost = 0
+        finalstepcost = cost[-1] # will out the last element in the list
+        cost.append(0)
+        
+        idx = (cost_length + 1) - 3
+        minimumCost = None
+        
+        while idx >= 0:
+            value1 = cost[idx] + finalstepcost
+            value2 = cost[idx] + topcost
+            
+            topcost = finalstepcost
+            
+            if value1 <= value2:
+                finalstepcost = value1
+            else:
+                finalstepcost = value2
+            
+            if idx == 1 or idx == 0:
+                if minimumCost == None:
+                    minimumCost = finalstepcost
+                else:
+                    if finalstepcost < minimumCost:
+                        minimumCost = finalstepcost
+            idx = idx - 1
+        
+        print(minimumCost)
+        return minimumCost
+            
+                
+                
+                
+        
+        
