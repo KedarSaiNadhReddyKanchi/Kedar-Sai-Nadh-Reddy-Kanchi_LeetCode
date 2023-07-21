25fe50fb92fa79c85909f798ad1caf0d782d6ba4
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2023-07-21 20:48:59 +0530

    Time: 63 ms (35.54%), Space: 13.6 MB (63.99%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        character_list = list(s)
+        hashMap = {}
+        longest_substsring = []
+        character_list_length = len(character_list)
+        running_count = 0
+        max_count = 0
+        index = 0
+        
+        while index < character_list_length:
+            character = character_list[index]
+            if character not in hashMap:
+                hashMap[character] = 1
+                running_count = running_count + 1
+                longest_substsring.append(character)
+                if running_count > max_count:
+                    max_count = running_count
+                index = index + 1
+            else:
+                if running_count > max_count:
+                    max_count = running_count
+                removed_character = longest_substsring.pop(0)
+                hashMap.pop(removed_character)
+                # index = index - 1
+                running_count = running_count - 1
+        
+        print(longest_substsring)
+        print(max_count)
+        return max_count
