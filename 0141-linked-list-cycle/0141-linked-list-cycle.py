b36f3eae1ad5109f9e57255cd2ee86ff97c43e87
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2023-07-22 21:17:56 +0530

    Time: 52 ms (22.98%), Space: 20.1 MB (96.57%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -11,15 +11,49 @@ class Solution(object):
         :rtype: bool
         """
         
-        hash_map = {}
-        temp = head
-        index = 0
-        while temp:
-            if temp in hash_map:
+        # *************************************method 1*********************************
+        # using a HASH MAP
+        # hash_map = {}
+        # temp = head
+        # index = 0
+        # while temp:
+        #     if temp in hash_map:
+        #         return True
+        #     else:
+        #         # here we are storing the node reference address as the key and value pair
+        #         # this is because later on we can check if the same node reference number 
+        #         #   has been hit up again in the cycle and if so return True
+        #         hash_map[temp] = temp
+        #     temp = temp.next
+        # return False
+        
+        # *************************************method 2*********************************
+        # using slow and fast pointers
+        # the concept is that imagine that 
+        #   the slow pointer moves 1 step forward at a time
+        #   and the fast pointer moves 2 steps forward at a time
+        # then if there is a cycle in the linked list then
+        #   the slow and fast pointers will meet at one point of time
+        # but if there is no cycle then the fast pointer will be the first to reach null node
+        
+        if head == None:
+            return False
+        
+        slow = head
+        if head.next == None or head.next.next == None:
+            fast = None
+        else:
+            fast = head.next.next 
+        
+        
+        while fast != None:
+            if slow == fast:
                 return True
+            slow = slow.next
+            if fast.next == None or fast.next.next == None:
+                fast = None
             else:
-                hash_map[temp] = temp
-            temp = temp.next
+                fast = fast.next.next
         return False
         
             
