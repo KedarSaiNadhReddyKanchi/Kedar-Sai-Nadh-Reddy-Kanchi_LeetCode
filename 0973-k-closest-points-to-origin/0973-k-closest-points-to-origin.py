4ba082b6ba630922e199c29588987554980d80bb
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2023-07-22 14:45:16 +0530

    Time: 793 ms (77.66%), Space: 22.6 MB (62.44%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,4 +1,3 @@
-import math 
 class Solution:
     
     def calculateEuclideanDistance(self , point):
@@ -6,32 +5,19 @@ class Solution:
         return math.sqrt(((xcor * xcor) + (ycor * ycor)))
     
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        
-        hashmap = {}
-        
+        N = len(points)
+        dists = []
         for point in points:
-            distance = self.calculateEuclideanDistance(point)
-            if distance not in hashmap:
-                hashmap[distance] = []
-            hashmap[distance].append(point)
-        # hashmap[3.1622776601683795].append([2,3])
-        
-        keysList = [key for key in hashmap]
-        keysList.sort()
-        results = []
-        count = 0
-        print(keysList)
-        for key in keysList:
-            for point in hashmap[key]:
-                results.append(point)
-                count = count + 1
-                if count == k:
-                    break
-            if count == k:
-                    break
-        print(results)
-        return results
+            dists.append(self.calculateEuclideanDistance(point))
             
+        dists.sort()
+        print(dists)
+        distK = dists[k-1];
+        print(distK)
         
+        ans = []
+        for point in points:
+            if self.calculateEuclideanDistance(point) <= distK:
+                ans.append(point)
         
-        
+        return ans
