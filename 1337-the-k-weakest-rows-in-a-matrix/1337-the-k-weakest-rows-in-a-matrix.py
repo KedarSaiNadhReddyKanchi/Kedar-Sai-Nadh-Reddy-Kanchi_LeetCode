33c623e5fb12cdb3bff0765b6756d8f707e6dbc3
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2023-08-18 23:42:11 -0700

    Time: 109 ms (80.30%), Space: 16.6 MB (75.47%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -2,23 +2,34 @@ import heapq
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
         
-        minheap = []
-        heapq.heapify(minheap)
+        reverseMinheap = []
+        heapq.heapify(reverseMinheap)
+        rowLength = len(mat[0])
+        
+        def binarySearch(row):
+            # efficient way of finding the first position of 0 instead of performing linear search
+            start = 0
+            end = rowLength
+            while start < end:
+                mid = start + int((end - start) / 2)
+                if row[mid] == 1:
+                    start = mid + 1
+                else:
+                    end = mid
+            return start
         
         for pos , row in enumerate(mat):
-            try:
-                position = row.index(0)
-                soliders = position
-                heapq.heappush(minheap , [soliders , pos])
-            except:
-                soliders = len(row)
-                heapq.heappush(minheap , [soliders , pos])
+            position = binarySearch(row)
+            soliders = position
+            heapq.heappush(reverseMinheap , [(-1 * soliders) , (-1 * pos)])
+            if len(reverseMinheap) > k:
+                heapq.heappop(reverseMinheap)
             
         result = []
         while k > 0:
-            popnum = heapq.heappop(minheap)
-            result.append(popnum[1])
+            popnum = heapq.heappop(reverseMinheap)
+            result.append((-1 * popnum[1]))
             k = k - 1
         
-        return result
+        return result[::-1]
                 
