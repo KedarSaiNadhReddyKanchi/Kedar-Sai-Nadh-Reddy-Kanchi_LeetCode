471b48c4768b84ee6af91a81f807c5e860e22b4f
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2023-08-25 18:33:10 -0700

    Time: 48 ms (25.86%), Space: 16.4 MB (31.13%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,17 +1,56 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         
-        first_characterin_needle = needle[0]
+        # need to implement sliding window concept
+        needle_pointer = 0
+        last_haystack_pointer = -1
         needle_length = len(needle)
+        haystack_length = len(haystack)
+        substring_length_found = 0
+        haystack_pointer = 0
         
-        haystackmap = {}
-        for index , haystackcharacter in enumerate(haystack):
-            if haystackcharacter ==  first_characterin_needle:
-                haystackmap[index] = "" + haystackcharacter
+        while haystack_pointer < haystack_length :
+            
+            if haystack[haystack_pointer] == needle[needle_pointer]:
+                if needle_pointer == 0:
+                    last_haystack_pointer = haystack_pointer
+                    print(last_haystack_pointer)
+                    
+                needle_pointer = needle_pointer + 1
+                substring_length_found = substring_length_found + 1
+                print("substring_length_found = " , substring_length_found)
+                if substring_length_found == needle_length:
+                    return last_haystack_pointer
+                else:
+                    haystack_pointer = haystack_pointer + 1
+            else:
+                needle_pointer = 0
+                substring_length_found = 0
+                if last_haystack_pointer != -1:
+                    haystack_pointer = last_haystack_pointer + 1
+                    last_haystack_pointer = -1
+                else:
+                    haystack_pointer = haystack_pointer  + 1
         
-        for index in haystackmap:
-            substring1 = haystack[index : (index + needle_length)]
-            if substring1 == needle:
-                return index
         return -1
+                
+                
+          
+            
         
+        # brute force
+#         first_characterin_needle = needle[0]
+#         needle_length = len(needle)
+        
+#         haystackmap = {}
+#         for index , haystackcharacter in enumerate(haystack):
+#             if haystackcharacter ==  first_characterin_needle:
+#                 haystackmap[index] = "" + haystackcharacter
+        
+#         for index in haystackmap:
+#             substring1 = haystack[index : (index + needle_length)]
+#             if substring1 == needle:
+#                 return index
+#         return -1
+        
+    
