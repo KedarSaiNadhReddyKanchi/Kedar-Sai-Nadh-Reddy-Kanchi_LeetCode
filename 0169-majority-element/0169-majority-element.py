5b4e90f138a5c9e68acc088ce036bee942bdc4e3
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2023-09-01 22:13:38 -0700

    Time: 160 ms (52.03%), Space: 17.9 MB (24.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,23 +1,37 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
         
+        total_count = 0
         hashmap = {}
-        maxcountnum = -1
-        maxcount = int(len(nums) / 2)
+        
+        high_number = None
+        high_count = None
         
         for num in nums:
             if num in hashmap:
                 hashmap[num] = hashmap[num] + 1
             else:
                 hashmap[num] = 1
-            
-            if hashmap[num] > maxcount:
-                maxcountnum = num
-        
-        return maxcountnum
                 
+            if ((high_number is None) and (high_count is None)):
+                high_number = num
+                high_count = hashmap[num]
+            else:
+                if hashmap[num] > high_count:
+                    high_number = num
+                    high_count = hashmap[num]
+            
+            total_count = total_count + 1
+            
+        limit = int(total_count / 2)
+        # print("total count is = ", total_count)
+        # print("high_number = ", high_number)
+        # print("high_count is = ", high_count)
+        # print(hashmap)
+        if high_count > limit:
+            return high_number
+                
+        return 0
+                
+            
         
