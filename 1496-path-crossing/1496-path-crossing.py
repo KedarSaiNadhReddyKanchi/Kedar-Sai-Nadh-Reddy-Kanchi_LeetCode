8a189d83bf13704a8071c1cee3ec28b1f8b3b7f1
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2024-02-06 18:29:52 -0700

    Time: 39 ms (34.94%), Space: 16.7 MB (97.95%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        
+        hash_map = {
+            "0,0": 1
+        }
+        pathlist = list(path)
+        
+        x = 0
+        y = 0
+        overlap = False
+        
+        for direction in pathlist:
+            if direction == "N":
+                y = y + 1
+            elif direction == "E":
+                x = x + 1
+            elif direction == "S":
+                y = y - 1
+            else:
+                x = x - 1
+            
+            x_temp = x
+            y_temp = y
+            point = "" + str(x_temp) + "," + str(y_temp)
+            print(point)
+            if point not in hash_map:
+                hash_map[point] = 1
+            else:
+                overlap = True
+                break
+        
+        return overlap
+            
+            
+                
+        
