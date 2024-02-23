63faf4b31df700deba7160d0404f115edc742154
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2024-02-22 18:29:05 -0700

    Time: 3163 ms (5.01%), Space: 20.8 MB (38.63%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -2,12 +2,14 @@ class NumArray:
 
     def __init__(self, nums: List[int]):
         self.nums = nums
-        
+        self.hashmap = {}
+        for position, num in enumerate(nums):
+            self.hashmap[position] = num
 
     def sumRange(self, left: int, right: int) -> int:
         totalsum = 0
         for position in range(left , (right + 1)):
-            totalsum = totalsum + self.nums[position]
+            totalsum = totalsum + self.hashmap[position]
         return totalsum
         
 
