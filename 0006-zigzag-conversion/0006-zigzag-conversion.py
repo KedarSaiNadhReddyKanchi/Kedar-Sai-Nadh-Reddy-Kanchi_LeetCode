6243c959f6ffcb5ab5f8e7b3cae5ffe7a0f9daca
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2024-03-07 14:20:54 -0700

    Time: 43 ms (91.30%), Space: 16.7 MB (52.74%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,61 +1,28 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-
         slength = len(s)
         
         if numRows == 1 or numRows >= slength:
             return s
-           
-        slist = list(s)
-        rows = numRows
-        columns = slength
-        if columns == 0:
-            columns = 1
-        arr = [[] for _ in range(rows)]
-        for i in range(rows):
-            arr[i] = [None] * columns
+    
+        stringcopy = [[] for _ in range(numRows)]
+        #print(stringcopy)
         
-        start = 0
-        end = 0
+        position = 0
         trend = "down"
-        flag = False
         
-        for letter in slist:
-            if start == (numRows):
-                trend = "up"
-                start = start - 1
-            
-            elif start == 0:
+        for letter in s:
+            stringcopy[position].append(letter)
+            if position == 0:
                 trend = "down"
-                if flag:
-                    start = start + 1
-                
+            elif position == (numRows - 1):
+                trend = "up"
+            
             if trend == "down":
-                arr[start][end] = letter
-                start = start + 1
-            
-            elif trend == "up":
-                start = start - 1
-                if start < 0:
-                    start = 0
-                end = end + 1
-                arr[start][end] = letter
-        
-            flag = True
-        
-        finalstring = ""
-        rowcount  = 0
-        while rowcount < numRows:
-            temp = 0
-            while temp <= end:
-                letter = arr[rowcount][temp]
-                if letter is not None:
-                    finalstring = finalstring + letter
-                temp = temp + 1
-            rowcount = rowcount + 1
+                position = position + 1
+            else:
+                position = position - 1
 
-        return finalstring
-                
-                
-            
         
+        #print(stringcopy)
+        return ''.join(''.join(row) for row in stringcopy)
