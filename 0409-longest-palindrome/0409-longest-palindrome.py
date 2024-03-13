5de0cae533fefb00f1eb1eef06d8a59797f54c92
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2024-03-13 14:13:56 -0700

    Time: 34 ms (77.16%), Space: 16.7 MB (47.39%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        hashmap = {}
+        evensum = 0
+        oddsum = 0
+        
+        for character in s:
+            if character not in hashmap:
+                hashmap[character] = 1
+                oddsum = oddsum + 1
+            else:
+                hashmap[character] = hashmap[character] + 1
+                if ((hashmap[character]) % 2) == 0:
+                    evensum = evensum + 2
+                    oddsum = oddsum - 1
+                else:
+                    oddsum = oddsum + 1
+        
+        print(hashmap)
+        print(evensum)
+        print(oddsum)
+        if oddsum > 0:
+            return evensum + 1
+        else:
+            return evensum
+
+        
