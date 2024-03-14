2a75a86495de73a3aaa884ae3b3b7e5b9e840574
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2024-03-14 11:57:13 -0700

    Time: 37 ms (41.99%), Space: 16.6 MB (39.45%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -7,32 +7,17 @@ class Solution:
         if num == 0:
             return 1
         
-        binary_num = []
         reversed_binary_num = []
         
         while num > 0:
             remainder = num % 2
-            binary_num.append(remainder)
             num = (int)(num / 2)
             
             if remainder == 1:
                 reversed_binary_num.append(0)
             else:
                 reversed_binary_num.append(1)
-    
-        print(binary_num)
-        print(reversed_binary_num)
-        
-#         binary_num_length = len(binary_num)
-#         position = binary_num_length - 1
-#         while position >= 0:
-#             if binary_num[position] == 1:
-#                 binary_num[position] = 0
-#             else:
-#                 binary_num[position] = 1
-#             position = position - 1
-#         print(binary_num)
-        
+
         complement = 0
         multiplier = 0
 
@@ -40,8 +25,5 @@ class Solution:
             value = bit * math.pow(2, multiplier)
             complement = complement + value
             multiplier = multiplier + 1
-            print(complement)
-        
-        print()
-        print(complement)
+
         return (int)(complement)
