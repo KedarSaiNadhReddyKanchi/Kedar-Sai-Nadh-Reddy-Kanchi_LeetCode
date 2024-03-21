e6a3556826071d8b8d632527bdc93086c258ef65
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2024-03-20 19:14:15 -0700

    Time: 94 ms (27.91%), Space: 19.1 MB (14.58%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,42 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        
+        left = 0
+        right = 0 
+        array = []
+        problematic_indices = []
+        slist = list(s)
+        
+        for position, character in enumerate(s):
+            if character == "(":
+                left = left + 1
+                array.append(["(", position])
+            elif character == ")":
+                # right = right + 1
+                # array.append([")", position])
+                if left == 0:
+                    problematic_indices.append(position)
+                else:
+                    if left > 0:
+                        array.pop()
+                        left = left - 1
+                    
+            else:
+                continue
+        
+        # print(f"left = {left} and right = {right}")
+        print(array)
+        print(problematic_indices)
+        
+        if left != 0:
+            for pair in array:
+                position = pair[1]
+                slist[position] = ""
+        
+        for index in problematic_indices:
+            slist[index] = ""
+        
+        print(slist)
+        result = "".join(slist)
+        return result
+        
