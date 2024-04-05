0451ed9fc8d7bd66c582d4b2bd55400ef029ffe5
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2024-04-05 11:36:34 -0700

    Time: 44 ms (17.93%), Space: 16.6 MB (46.83%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,41 +1,21 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        hashmap = {}
-        for position, character in enumerate(s):
-            # ADD THE CHARACTER INTO THE HASHMAP
-            hashmap[position] = character
-            
-            # RETRIEVE THE PREVIOUS POSITION
-            previous_position = position - 1
-            
-            # IF THE PREVIOUS POSITION IN HASHMAP THEN CHECK FOR THE PROBLEM CONDITION
-            if previous_position in hashmap:
-                previousvalue = hashmap[previous_position]
-                currentvalue = character
-                finalcondition = self.checkCondition(previousvalue , currentvalue)
-                
-                if finalcondition:
-                    del hashmap[previous_position]
-                    del hashmap[position]
-                                
-        print(hashmap)
-        
         result = []
         count = 0
-        for key in hashmap:
+        for character in s:
             if count == 0:
-                result.append(hashmap[key])
+                result.append(character)
                 count = count + 1
             else:
                 poppedvalue = result.pop()
                 count = count - 1
                 
-                finalcondition = self.checkCondition(poppedvalue , hashmap[key])
+                finalcondition = self.checkCondition(poppedvalue , character)
                 if finalcondition:
                     continue
                 else:
                     result.append(poppedvalue)
-                    result.append(hashmap[key])
+                    result.append(character)
                     count = count + 2
         
         print(result)
@@ -61,4 +41,3 @@ class Solution:
             return True
         
         return False
-        
