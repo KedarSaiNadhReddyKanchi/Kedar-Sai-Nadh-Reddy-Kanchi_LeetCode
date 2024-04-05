f674295f633116de3d8cc2c96054f1ca6e2a9daa
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2024-04-05 14:06:04 -0700

    Time: 637 ms (7.50%), Space: 30.7 MB (80.22%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
@@ -0,0 +1,49 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        freq = {}
+        countone = {}
+        total_count_of_numbers_in_countone = 0
+        
+        for num in nums:
+            if num not in freq:
+                freq[num] = 1
+                countone[num] = 1
+                total_count_of_numbers_in_countone = total_count_of_numbers_in_countone + 1
+            else:
+                freq[num] = freq[num] + 1
+                if num in countone:
+                    del countone[num]   
+                    total_count_of_numbers_in_countone = total_count_of_numbers_in_countone - 1
+        
+        print(freq)
+        print(countone)
+        print(total_count_of_numbers_in_countone)
+        
+        if total_count_of_numbers_in_countone != 0:
+            return -1
+        
+        total_deletion_operation_counts = 0
+        
+        for key in freq:
+            value = freq[key]
+            operationscount = 0
+            
+            if value % 3 == 0:
+                number = (int)(value / 3)
+                operationscount = operationscount + number
+            
+            elif value % 3 == 2:
+                number_by_3 = (int)(value / 3)
+                operationscount = operationscount + number_by_3 + 1
+            
+            elif value % 3 == 1:
+                temp = value - 2
+                temp_by_3 = (int)(temp / 3)
+                operationscount = operationscount + temp_by_3 + 1 + 1
+            
+            total_deletion_operation_counts = total_deletion_operation_counts + operationscount
+            
+            print(f"for key = {key} with freq = {value} needs deletionOperations = {operationscount}")
+
+        return total_deletion_operation_counts
+            
