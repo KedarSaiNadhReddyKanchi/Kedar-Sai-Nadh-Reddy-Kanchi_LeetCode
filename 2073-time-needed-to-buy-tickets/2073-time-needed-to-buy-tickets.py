d5ea8b034dec4eca7b69711389243b9aebf637df
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2024-04-12 18:25:39 -0700

    Time: 26 ms (99.27%), Space: 16.7 MB (12.53%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,23 +1,20 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        
         tickets_needed_to_buy = tickets[k]
         time_count = 0
+        lesser_value_count = 0
         
-        while (tickets_needed_to_buy != 0):
-            for position , perPersonTicketCount in enumerate(tickets):
-                if perPersonTicketCount == 0:
-                    continue
-                else:
-                    tickets[position] = perPersonTicketCount - 1
-                    time_count = time_count + 1
-                    if position == k:
-                        tickets_needed_to_buy = tickets_needed_to_buy - 1
-                        if tickets_needed_to_buy == 0:
-                            break
+        for position , ticketCount in enumerate(tickets):
+            if ticketCount >= tickets_needed_to_buy:
+                time_count = time_count + (tickets_needed_to_buy - 1)
+            else:
+                time_count = time_count + ticketCount
+                if position < k:
+                    lesser_value_count = lesser_value_count + 1
         
-        return time_count
-
-        
-                        
+        print(f"time_count = {time_count} and lesser_value_count = {lesser_value_count}")
+        total_time_count = time_count + 1 + k - lesser_value_count
+        print(f"total_time_count = {total_time_count}")
+        return total_time_count
+                
         
