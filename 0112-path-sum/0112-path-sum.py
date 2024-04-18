5c493db691320c17f62048542f3f851778208faf
KedarSaiNadhReddyKanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2024-04-17 20:45:23 -0700

    Time: 53 ms (6.45%), Space: 17.6 MB (8.16%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return 0
+        
+        if root.left is None and root.right is None:
+            if root.val == targetSum:
+                return True
+            return False
+        
+        queue = deque()
+        queue.append((root , 1, 0))
+        size = 1
+        totalSum = 0
+        
+        while size > 0:
+            poppednode, level, runningvalue = queue.popleft()
+            size = size - 1
+            print(f"for node = {poppednode.val} at level = {level} the runningvalue = {runningvalue}")
+            
+            if poppednode.left is not None:
+                left_running_value = runningvalue + poppednode.val
+                queue.append((poppednode.left, (level + 1), left_running_value))
+                size = size + 1
+            
+            if poppednode.right is not None: 
+                right_running_value = runningvalue + poppednode.val
+                queue.append((poppednode.right, (level + 1), right_running_value))
+                size = size + 1
+                
+            if poppednode.left is None and poppednode.right is None:
+                current_running_value = runningvalue + poppednode.val
+                totalSum = totalSum + current_running_value
+                print(f"for leaf node = {poppednode.val} the total sum at that point is = {totalSum}")
+                if totalSum == targetSum:
+                    return True
+                else:
+                    totalSum = 0
+    
+        return False
+                
+        
