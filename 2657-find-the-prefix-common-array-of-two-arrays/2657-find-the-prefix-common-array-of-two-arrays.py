2661d86c70574415d3b54e8b082311723a4d2e6c
Kedar Sai Nadh Reddy Kanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2025-01-13 19:21:51 -0500

    Time: 10 ms (57.81%), Space: 17.6 MB (45.05%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
@@ -2,20 +2,23 @@ class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
         map_A = {}
         map_B = {}
-        map_C = {}
+        # map_C = {}
+        count = 0
         result = []
 
         for value_A, value_B in zip(A, B):
             map_A[value_A] = True
             if value_A in map_B:
-                map_C[value_A] = True
+                count = count + 1
+                # map_C[value_A] = True
             
             map_B[value_B] = True
             if value_B in map_A:
-                map_C[value_B] = True
+                count = count + 1
+                # map_C[value_B] = True
             
             # print(map_C)
-            result.append(len(map_C))
+            result.append(count)
         
         return result
             
