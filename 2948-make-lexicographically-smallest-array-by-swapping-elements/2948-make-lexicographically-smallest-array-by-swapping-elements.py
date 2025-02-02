6b1a82ea01e53e104ea4bc4f1ae75f06eee320b0
Kedar Sai Nadh Reddy Kanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2025-02-01 21:20:40 -0500

    Time: 638 ms (9.05%), Space: 106.2 MB (5.66%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,33 +1,40 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+        
+        groups = deque([])
+        
+        # to form the groups you need to first the sort the given array and check which groups they belong to
+        new_nums = [[num , position] for position , num in enumerate(nums)]
+        new_nums.sort()
 
-        position_map = {}
-        minheap = []
-        heapq.heapify(minheap)
-        result = [0] * len(nums)
-        unoccupied_positions_map = {}
+        position_group_map = {}
+        
+        # now iterate and form the groups
+        for num, position in new_nums:
+            if len(groups) == 0:
+                new_group = deque([num])
+                groups.append(new_group)
+            else:
+                last_group = groups[-1]
+                last_group_element = last_group[-1]
+                difference = abs(num - last_group_element)
+                if difference <= limit:
+                    groups[-1].append(num)
+                else:
+                    new_group = deque([num])
+                    groups.append(new_group)
+            position_group_map[position] = len(groups)
+        
+        # since the iteration was done on a sorted nums array - the groups formed will by default be sorted as well.
+        # print(groups)
+        # print(position_group_map)
 
+        # now you have the groups and the in which group each of the position is in. 
+        # following that just fill in the values by popping out the values from the left of the each group
         for position, num in enumerate(nums):
-            heapq.heappush(minheap, (num, position))
-            position_map[position] = num
-            unoccupied_positions_map[position] = True
-
-        while len(minheap) > 0:
-            popped_out_values = heapq.heappop(minheap)
-            number, index = popped_out_values[0], popped_out_values[1]
-            print(f"number = {number} index = {index} ")
-            for unoccupied_index in unoccupied_positions_map:
-                if unoccupied_positions_map[unoccupied_index]:
-                    existing_number = position_map[unoccupied_index]
-                    absolute_difference = abs(existing_number - number)
-                    if absolute_difference <= limit:
-                        position_map[unoccupied_index] = number
-                        position_map[index] = existing_number
-                        result[unoccupied_index] = number
-                        unoccupied_positions_map[unoccupied_index] = False
-                        # print(result)
-                        # print(position_map)
-                        # print()
-                        break
-
-        return result
+            assigned_group = position_group_map[position]
+            # least_possible_value_to_be_filled_at_this_point_from_the_respective_group is
+            least_possible_value = groups[assigned_group - 1].popleft()
+            nums[position] = least_possible_value
+        
+        return nums
