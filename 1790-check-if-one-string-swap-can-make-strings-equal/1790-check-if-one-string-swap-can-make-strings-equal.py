c126c0b7c60ff6e702f4289e560ede871eedaafd
Kedar Sai Nadh Reddy Kanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2025-02-04 21:03:52 -0500

    Time: 4 ms (6.82%), Space: 17.7 MB (47.15%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -0,0 +1,45 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        changes = 0
+        s1_map = {}
+        s2_map = {}
+        
+        for character1 , character2 in zip(s1 , s2):
+            if character1 == character2:
+                changes = changes + 0
+            else:
+                changes = changes + 1
+                if changes == 3:
+                    return False
+                
+            if character1 not in s1_map:
+                s1_map[character1] = 0
+            
+            if character2 not in s2_map:
+                s2_map[character2] = 0
+            
+            s1_map[character1] = s1_map[character1] + 1
+            s2_map[character2] = s2_map[character2] + 1
+        
+        if changes == 0:
+            return True # no changes to be made in any one of the strings
+        
+        for character1 , character2 in zip(s1 , s2):
+            if character1 not in s2_map:
+                # unknown character in s1 that is not there in s2 so return False
+                return False
+            
+            if character2 not in s1_map:
+                # unknown character in s2 that is not there in s1 so return False
+                return False
+            
+            # now at this point it means that both the characters are present in each other
+            # but now comapre frequencies if required but I do not think it would come to this point
+            character_1_frequency = (s1_map[character1] == s2_map[character1])
+            character_2_frequency = (s1_map[character2] == s2_map[character2])
+            final_condition = (character_1_frequency and character_2_frequency)
+            if not final_condition:
+                # difference in frequencies so return False
+                return False
+
+        return True
