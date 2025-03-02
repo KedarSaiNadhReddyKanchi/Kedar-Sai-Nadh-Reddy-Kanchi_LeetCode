8a9fe479c1c3baecf6072cf13b1e0e93a741dc66
Kedar Sai Nadh Reddy Kanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2025-03-01 19:35:54 -0500

    Time: 0 ms (100%), Space: 18.2 MB (17.98%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
@@ -1,20 +1,38 @@
-from sortedcontainers import SortedDict
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        ids = SortedDict()
+        pointer_1 = 0
+        pointer_2 = 0
 
-        for id1 , value in nums1:
-            ids[id1] = value
-        
-        for id2 , value in nums2:
-            if id2 not in ids:
-                ids[id2] = value
-            else:
-                ids[id2] = ids[id2] + value
-        
-        # print(ids)
+        nums1_size = len(nums1)
+        nums2_size = len(nums2)
+
         result = []
-        for unique_id in ids:
-            result.append([unique_id , ids[unique_id]])
-        return result
 
+        while (pointer_1 < nums1_size) and (pointer_2 < nums2_size):
+            id_at_pointer_1 , value_1 = nums1[pointer_1]
+            id_at_pointer_2 , value_2 = nums2[pointer_2]
+            if id_at_pointer_1 == id_at_pointer_2:
+                ids_sum = value_1 + value_2
+                result.append([id_at_pointer_1 , ids_sum])
+                pointer_1 = pointer_1 + 1
+                pointer_2 = pointer_2 + 1
+            else:
+                if id_at_pointer_1 < id_at_pointer_2:
+                    result.append([id_at_pointer_1 , value_1])
+                    pointer_1 = pointer_1 + 1
+                else:
+                    result.append([id_at_pointer_2 , value_2])
+                    pointer_2 = pointer_2 + 1
+        
+        while (pointer_1 >= nums1_size) and (pointer_2 < nums2_size):
+            id_at_pointer_2 , value_2 = nums2[pointer_2]
+            result.append([id_at_pointer_2 , value_2])
+            pointer_2 = pointer_2 + 1
+        
+        while (pointer_2 >= nums2_size) and (pointer_1 < nums1_size):
+            id_at_pointer_1 , value_1 = nums1[pointer_1]
+            result.append([id_at_pointer_1 , value_1])
+            pointer_1 = pointer_1 + 1
+        
+        print(nums1_size , nums2_size, pointer_1, pointer_2)
+        return result
