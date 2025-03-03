3a9fedb6112965da96638ad3637b2216261a67e0
Kedar Sai Nadh Reddy Kanchi <119361176+KedarSaiNadhReddyKanchi@users.noreply.github.com>
2025-03-02 19:49:53 -0500

    Time: 81 ms (8.23%), Space: 34.9 MB (23.77%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,5 +1,51 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        # optimal one pass solution compared to the 2 pass solution described below
+        nums_size = len(nums)
+        left_pointer = 0
+        right_pointer = nums_size - 1
+
+        move_left = 0 
+        move_right = nums_size - 1
+        result = [None] * nums_size
+
+        while (move_left < nums_size) and (move_right >= 0):
+            left_value = nums[move_left]
+            right_value = nums[move_right]
+
+            # update the left portion of the result array 
+            # with the sequential occurences of values less than the pivot
+            # in the order they appear
+            if left_value < pivot:
+                result[left_pointer] = left_value
+                # update the position for the left portion of the array
+                # at which the next value should be placed
+                left_pointer = left_pointer + 1
+            
+            # update the right portion of the result array 
+            # with the sequential occurences of values greater than the pivot
+            # in the order they appear from the right to left order
+            if right_value > pivot:
+                result[right_pointer] = right_value
+                # update the position for the right portion of the array
+                # at which the next value should be placed
+                right_pointer = right_pointer - 1
+            
+            # update the move pointers that are moving across the nums array
+            move_left = move_left + 1
+            move_right = move_right - 1
+        
+        # now time to fill in all the values equal to the pivot that present in the nums array
+        # this can be done by bridging any gap between the left_pointer and the right_pointer
+        while left_pointer <= right_pointer:
+            result[left_pointer] = pivot
+            left_pointer = left_pointer + 1
+        
+        return result
+
+        # MY SOLTUION WHICH GOT ACCEPTED BY WITH 2-PASS 
+        # TIME COMPLEXITY NOT THAT GREAT
+
         less = 0
         greater = 0
         equal = 0
@@ -15,10 +61,14 @@ class Solution:
         
         result = [None] * size
         pointer = less
+        check = (equal == size)
         while equal > 0:
             result[pointer] = pivot
             pointer = pointer + 1
             equal = equal - 1
+        
+        if check:
+            return result
         
         left = 0
         right = pointer
